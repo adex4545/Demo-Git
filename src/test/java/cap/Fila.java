f6a658baab6579f3af7a9747775408426d010647
adex4545 <adebolaa015@gmail.com>
2021-06-28 12:15:05 +0100

    First commit

---
+++ src/test/java/cap/Fila.java
@@ -0,0 +1,17 @@
+package cap;
+
+public class Fila {
+
+	public static void main(String[] args) {
+		
+        String hungry ="yes";
+		
+		if(hungry == "yes")
+			System.out.println("must eat something");
+		else {
+			System.out.println("continue with work");
+		}
+
+	}
+
+}
